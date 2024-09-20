5736a479c5e204daf30172e0d75c1b1631085dd3
Cecchy2 <dariocecchinato@gmail.com>
2024-09-20 14:03:58 +0200

    creato AuthController,

---
+++ src/main/java/dariocecchinato/s19l5_gestione_eventi_project/controllers/AuthController.java
@@ -0,0 +1,37 @@
+package dariocecchinato.s19l5_gestione_eventi_project.controllers;
+
+import dariocecchinato.s19l5_gestione_eventi_project.exceptions.BadRequestException;
+import dariocecchinato.s19l5_gestione_eventi_project.payloads.NewUtenteRespDTO;
+import dariocecchinato.s19l5_gestione_eventi_project.payloads.OrganizzatorePayloadDTO;
+import dariocecchinato.s19l5_gestione_eventi_project.services.AuthService;
+import dariocecchinato.s19l5_gestione_eventi_project.services.OrganizzatoriService;
+import dariocecchinato.s19l5_gestione_eventi_project.services.UtentiService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    @Autowired
+    private AuthService authService;
+    @Autowired
+    private OrganizzatoriService organizzatoriService;
+
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.CREATED)
+    public NewUtenteRespDTO save(@RequestBody @Validated OrganizzatorePayloadDTO body, BindingResult validationResult){
+        if(validationResult.hasErrors()){
+            String messages= validationResult.getAllErrors().stream()
+                    .map(objectError -> objectError.getDefaultMessage())
+                    .collect(Collectors.joining(". "));
+            throw new BadRequestException("Errori nel Payload. " + messages);
+        }else{
+            return new NewUtenteRespDTO(this.organizzatoriService.save(body).getId());
+        }
+    }
+}

1708707b548db2c2cc51096a80561cd7df627345
Cecchy2 <dariocecchinato@gmail.com>
2024-09-20 15:39:41 +0200

    aggiunto metodo get findAll

--- src/main/java/dariocecchinato/s19l5_gestione_eventi_project/controllers/UtentiController.java
+++ src/main/java/dariocecchinato/s19l5_gestione_eventi_project/controllers/UtentiController.java
@@ -1,9 +1,22 @@
 package dariocecchinato.s19l5_gestione_eventi_project.controllers;
 
+import dariocecchinato.s19l5_gestione_eventi_project.entities.Utente;
+import dariocecchinato.s19l5_gestione_eventi_project.services.UtentiService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/utenti")
 public class UtentiController {
+    @Autowired
+    private UtentiService utentiService;
+
+    @GetMapping
+    public Page<Utente> findAllDipendenti(@RequestParam(defaultValue = "0")int page, @RequestParam(defaultValue = "10") int size, @RequestParam(defaultValue = "nome")String sortby){
+        return this.utentiService.findAll(page, size, sortby);
+    }
 }
--- src/main/java/dariocecchinato/s19l5_gestione_eventi_project/services/UtentiService.java
+++ src/main/java/dariocecchinato/s19l5_gestione_eventi_project/services/UtentiService.java
@@ -9,6 +9,10 @@ import dariocecchinato.s19l5_gestione_eventi_project.payloads.OrganizzatorePaylo
 import dariocecchinato.s19l5_gestione_eventi_project.payloads.UtentePayloadDTO;
 import dariocecchinato.s19l5_gestione_eventi_project.repositories.UtentiRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -23,6 +27,12 @@ public class UtentiService {
 
     public Utente findById(UUID userId) {
         return this.utentiRepository.findById(userId).orElseThrow(() -> new NotFoundException(userId));
+    }
+
+    public Page<Utente> findAll(int page, int size, String sortby){
+        if (page > 10) page = 10;
+        Pageable pageable= PageRequest.of(page,size, Sort.by(sortby));
+        return utentiRepository.findAll(pageable);
     }
 
     public Utente findUtenteByEmail(String email){
